ceafe50972ad5c5fc79ab0e754ea627bf1950e22
Lulchenko Aleksey <LulchenkoAleksey@gmail.com>
2019-07-31 00:43:01 +0400

    Windows Installer added

--- DataBase/DataBaseFileHelper.cs
+++ DataBase/DataBaseFileHelper.cs
@@ -20,34 +20,5 @@ namespace Gorchun.DataBase
             string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, DATABASE_FILE_PARENT_FOLDER, DATABASE_FILENAME);
             return filePath;
         }
-
-        //private static void CreateClientDatabase(string destinationFileName)
-        //{
-        //    string sourceDbFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, DATABASE_FILE_PARENT_FOLDER, DATABASE_FILENAME);
-        //    if (File.Exists(sourceDbFile) == false)
-        //    {
-        //        throw new FileNotFoundException($"Source database file '{sourceDbFile}' doesn't exist.");
-        //    }
-
-        //    CreateDirectoryIfNotExists(Path.GetDirectoryName(destinationFileName));
-        //    FileInfo fileInfo = new FileInfo(sourceDbFile);
-        //    //_logger.Info("Creating a new database file...");
-        //    fileInfo.CopyTo(destinationFileName, overwrite: true);
-
-        //    if (File.Exists(destinationFileName) == false)
-        //    {
-        //        //_logger.Info($"Can't create database file '{destinationFileName}'.");
-        //        throw new FileNotFoundException($"An error occured while creating the database file {destinationFileName}");
-        //    }
-        //}
-
-        //private static void CreateDirectoryIfNotExists(string path)
-        //{
-        //    if (Directory.Exists(path) == false)
-        //    {
-        //        //_logger.Info($"Creating directory for client database: '{path}'");
-        //        Directory.CreateDirectory(path);
-        //    }
-        //}
     }
 }
